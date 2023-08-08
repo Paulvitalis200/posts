4a1a179bfd0378b53963aa98eb4485e0faa4fb28
Paul Otieno <vitalispaul48@live.com>
2023-08-08 06:56:13 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+
+// Create a form that 
+// Has - title, body and userId - number
+
+// When you hit submit, get the form values and send your request to the posts endpoint: 'https://jsonplaceholder.typicode.com/posts'
+
+// After you send the request and get a response, log the response in the terminal.
+
+// Here is an sample example of a post request using  fetch API
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+
+// The output would be as follows:
+
+// {
+//     id: 101,
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1
+//   }
+
+//  You can read more here
